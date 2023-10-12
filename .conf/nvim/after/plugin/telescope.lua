c70bfdc9df08c007a6f2ddcba5f7388542eed7f0
Kevin Montoya <kmontocam@outlook.com>
2023-10-12 16:12:15 -0600

    feat: prevent preview of large files

--- .conf/nvim/after/plugin/telescope.lua
+++ .conf/nvim/after/plugin/telescope.lua
@@ -8,8 +8,30 @@ if not actions_setup then
 	return nil
 end
 
+local previewers_setup, previewers = pcall(require, "telescope.previewers")
+if not previewers_setup then
+	return nil
+end
+
+local new_maker = function(filepath, bufnr, opts)
+	opts = opts or {}
+
+	filepath = vim.fn.expand(filepath)
+	vim.loop.fs_stat(filepath, function(_, stat)
+		if not stat then
+			return nil
+		end
+		if stat.size > 1048576 then
+			return nil
+		else
+			previewers.buffer_previewer_maker(filepath, bufnr, opts)
+		end
+	end)
+end
+
 telescope.setup({
 	defaults = {
+		buffer_previewer_maker = new_maker,
 		mappings = {
 			i = {
 				["<C-k>"] = actions.move_selection_previous,
