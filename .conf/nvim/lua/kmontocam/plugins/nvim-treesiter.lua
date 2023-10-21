fa242c50cdc187d0e105cd40e2d35d645b61e58a
Kevin Montoya <kmontocam@outlook.com>
2023-10-21 12:24:39 -0600

    feat: extended motions with treesitter objects

--- .conf/nvim/lua/kmontocam/plugins/nvim-treesiter.lua
+++ .conf/nvim/lua/kmontocam/plugins/nvim-treesiter.lua
@@ -53,41 +53,134 @@ return {
             enable = true,
             lookahead = true,
             keymaps = {
-              ["Aa"] = "@parameter.outer",
-              ["iA"] = "@parameter.inner",
-              ["Af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["Ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
+              ["a="] = {
+                query = "@assignment.outer",
+                desc = "Select outer part of an assignment",
+              },
+              ["i="] = {
+                query = "@assignment.inner",
+                desc = "Select inner part of an assignment",
+              },
+              ["l="] = {
+                query = "@assignment.lhs",
+                desc = "Select left hand side of an assignment",
+              },
+              ["r="] = {
+                query = "@assignment.rhs",
+                desc = "Select right hand side of an assignment",
+              },
+
+              ["a:"] = {
+                query = "@property.outer",
+                desc = "Select outer part of an object property",
+              },
+              ["i:"] = {
+                query = "@property.inner",
+                desc = "Select inner part of an object property",
+              },
+              ["l:"] = {
+                query = "@property.lhs",
+                desc = "Select left part of an object property",
+              },
+              ["r:"] = {
+                query = "@property.rhs",
+                desc = "Select right part of an object property",
+              },
+
+              ["aa"] = {
+                query = "@parameter.outer",
+                desc = "Select outer part of a parameter/argument",
+              },
+              ["ia"] = {
+                query = "@parameter.inner",
+                desc = "Select inner part of a parameter/argument",
+              },
+
+              ["ai"] = {
+                query = "@conditional.outer",
+                desc = "Select outer part of a conditional",
+              },
+              ["ii"] = {
+                query = "@conditional.inner",
+                desc = "Select inner part of a conditional",
+              },
+
+              ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+              ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+              ["am"] = {
+                query = "@call.outer",
+                desc = "Select outer part of a function call",
+              },
+              ["im"] = {
+                query = "@call.inner",
+                desc = "Select inner part of a function call",
+              },
+
+              ["af"] = {
+                query = "@function.outer",
+                desc = "Select outer part of a method/function definition",
+              },
+              ["if"] = {
+                query = "@function.inner",
+                desc = "Select inner part of a method/function definition",
+              },
+
+              ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
             },
           },
           move = {
             enable = true,
             set_jumps = true,
             goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]]"] = "@class.outer",
+              ["]m"] = { query = "@call.outer", desc = "Next function call start" },
+              ["]f"] = {
+                query = "@function.outer",
+                desc = "Next method/function def start",
+              },
+              ["]c"] = { query = "@class.outer", desc = "Next class start" },
+              ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+              ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
             },
             goto_next_end = {
-              ["]F"] = "@function.outer",
-              ["]["] = "@class.outer",
+              ["]M"] = { query = "@call.outer", desc = "Next function call end" },
+              ["]F"] = {
+                query = "@function.outer",
+                desc = "Next method/function def end",
+              },
+              ["]C"] = { query = "@class.outer", desc = "Next class end" },
+              ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+              ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
             },
             goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[["] = "@class.outer",
+              ["[m"] = { query = "@call.outer", desc = "Prev function call start" },
+              ["[f"] = {
+                query = "@function.outer",
+                desc = "Prev method/function def start",
+              },
+              ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+              ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+              ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
             },
             goto_previous_end = {
-              ["[F"] = "@function.outer",
-              ["[]"] = "@class.outer",
+              ["[M"] = { query = "@call.outer", desc = "Prev function call end" },
+              ["[F"] = {
+                query = "@function.outer",
+                desc = "Prev method/function def end",
+              },
+              ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+              ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+              ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
             },
           },
           swap = {
             enable = true,
             swap_next = {
-              ["Ai"] = "@parameter.inner",
+              ["<leader>nf"] = "@function.outer",
             },
             swap_previous = {
-              ["AI"] = "@parameter.inner",
+              ["<leader>pf"] = "@function.outer",
             },
           },
         },
