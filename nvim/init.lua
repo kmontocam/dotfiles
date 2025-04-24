fd4b617e94a4e18c8dc1d2090ea295e985919af4
Kevin Montoya <me@kmontocam.com>
2025-04-23 18:09:23 -0600

    feat: nvim 0.11 configuration changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,13 @@
+---@diagnostic disable-next-line: duplicate-set-field
+vim.deprecate = function() end
+
 require("kmontocam.core.keymaps")
 require("kmontocam.core.options")
 require("kmontocam.lazy")
+
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = {
+    current_line = true,
+  },
+})
