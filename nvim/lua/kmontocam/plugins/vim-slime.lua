4ff4853a09996f15785c39aab0342886069f15f6
Kevin Montoya <me@kmontocam.com>
2025-05-12 16:34:02 -0600

    feat: drop vim slime ctrl-c keybind in normal mode

--- nvim/lua/kmontocam/plugins/vim-slime.lua
+++ nvim/lua/kmontocam/plugins/vim-slime.lua
@@ -7,7 +7,7 @@ return {
 	  let g:slime_default_config = {"socket_name": "default", "target_pane": "{last}"}
 	  let g:slime_bracketed_paste = 1
 	]])
-    vim.keymap.del("v", "<C-c><C-c>")
+    vim.keymap.del({ "v", "n" }, "<C-c><C-c>")
     vim.keymap.set("n", "<Leader>oc", "<Plug>SlimeConfig", { desc = "Config vim-slime" })
     vim.keymap.set("n", "<Leader>op", "<Plug>SlimeParagraphSend", { desc = "Send paragraph to next tmux pane" })
     vim.keymap.set("v", "<Leader>oo", "<Plug>SlimeRegionSend", { desc = "Send selected to next tmux pane" })
