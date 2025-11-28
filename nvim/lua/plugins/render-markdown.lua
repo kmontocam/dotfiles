ac9d0cc7c85b169ebf43730cb6757a64c9b06139
Kevin Montoya <me@kmontocam.com>
2025-11-28 16:35:27 -0600

    feat: render markdown nvim plugin

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("render-markdown").setup({
+      completions = { lsp = { enabled = true } },
+    })
+  end,
+}
