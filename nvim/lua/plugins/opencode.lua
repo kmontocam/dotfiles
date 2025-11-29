dea0971122d5611c30e0e2506baeef4ed2c5cebc
Kevin Montoya <me@kmontocam.com>
2025-11-28 20:19:12 -0600

    feat: opencode nvim plugin

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -5,33 +5,19 @@ return {
   },
   config = function()
     vim.g.opencode_opts = {
-      -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      port = 4096,
+      provider = {
+        enabled = "tmux",
+        tmux = {},
+      },
     }
 
-    -- Required for `opts.events.reload`.
     vim.o.autoread = true
-
-    -- Recommended/example keymaps.
-    vim.keymap.set({ "n", "x" }, "<C-t>", function()
-      require("opencode").ask("@this: ", { submit = true })
-    end, { desc = "Ask opencode" })
     vim.keymap.set({ "n", "x" }, "<C-x>", function()
       require("opencode").select()
     end, { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "x" }, "ga", function()
+    vim.keymap.set({ "n", "x" }, "go", function()
       require("opencode").prompt("@this")
     end, { desc = "Add to opencode" })
-    vim.keymap.set({ "n", "t" }, "<C-.>", function()
-      require("opencode").toggle()
-    end, { desc = "Toggle opencode" })
-    vim.keymap.set("n", "<S-C-u>", function()
-      require("opencode").command("session.half.page.up")
-    end, { desc = "opencode half page up" })
-    vim.keymap.set("n", "<S-C-d>", function()
-      require("opencode").command("session.half.page.down")
-    end, { desc = "opencode half page down" })
-    -- You may want these if you stick with the opinionated "<C-t>" and "<C-x>" above — otherwise consider "<leader>o".
-    vim.keymap.set("n", "+", "<C-t>", { desc = "Increment", noremap = true })
-    vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
   end,
 }
