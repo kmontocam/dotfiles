6cdefd7b10965c24825273f61711b710687bcf5c
Kevin Montoya <me@kmontocam.com>
2025-11-28 18:50:35 -0600

    feat: rename yank relative path command keymap

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -19,7 +19,7 @@ vim.keymap.set("x", "<leader>p", [["_dp]], { desc = "Paste without copying into 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>d", [["+d]], { desc = "Cut to system clipboard" })
 
-vim.keymap.set("n", "<leader>gg", function()
+vim.keymap.set("n", "<leader>yg", function()
   local filepath = vim.fn.expand("%")
   vim.fn.setreg("+", filepath)
 end, { desc = "Copy relative file path to system clipboard" })
