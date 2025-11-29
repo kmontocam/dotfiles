e8735956f7ae0ea92e4209c176d1a2d772a6aa33
Kevin Montoya <me@kmontocam.com>
2025-11-28 19:08:52 -0600

    chore: keymaps for vim slime

--- nvim/lua/plugins/vim-slime.lua
+++ nvim/lua/plugins/vim-slime.lua
@@ -1,15 +1,18 @@
 return {
   "jpalardy/vim-slime",
   event = "VeryLazy",
+  init = function()
+    vim.g.slime_no_mappings = 1
+    vim.g.slime_dont_ask_default = 1
+  end,
   config = function()
     vim.g.slime_target = "tmux"
     vim.cmd([[
 	  let g:slime_default_config = {"socket_name": "default", "target_pane": "{last}"}
 	  let g:slime_bracketed_paste = 1
 	]])
-    vim.keymap.del({ "v", "n" }, "<C-c><C-c>")
-    vim.keymap.set("n", "<Leader>oc", "<Plug>SlimeConfig", { desc = "Config vim-slime" })
-    vim.keymap.set("n", "<Leader>op", "<Plug>SlimeParagraphSend", { desc = "Send paragraph to next tmux pane" })
-    vim.keymap.set("v", "<Leader>oo", "<Plug>SlimeRegionSend", { desc = "Send selected to next tmux pane" })
+    vim.keymap.set("n", "<leader>mc", "<Plug>SlimeConfig", { desc = "Config vim-slime" })
+    vim.keymap.set("n", "<leader>mp", "<Plug>SlimeParagraphSend", { desc = "Send paragraph to next tmux pane" })
+    vim.keymap.set("v", "<leader>mm", "<Plug>SlimeRegionSend", { desc = "Send selected to next tmux pane" })
   end,
 }
