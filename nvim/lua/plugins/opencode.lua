7b4db60ada38186e4d666ce16e938069ef6f309e
Kevin Montoya <me@kmontocam.com>
2025-12-01 09:25:54 -0700

    feat: opencode keymaps

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -4,8 +4,13 @@ return {
     { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
   },
   config = function()
+    -- generate dynamic port based on process ID to allow multiple nvim sessions
+    local base_port = 4096
+    local pid = vim.fn.getpid()
+    local dynamic_port = base_port + (pid % 1000)
+
     vim.g.opencode_opts = {
-      port = 4096,
+      port = dynamic_port,
       provider = {
         enabled = "tmux",
         tmux = {},
@@ -13,11 +18,24 @@ return {
     }
 
     vim.o.autoread = true
-    vim.keymap.set({ "n", "x" }, "<C-x>", function()
-      require("opencode").select()
-    end, { desc = "Execute opencode action…" })
-    vim.keymap.set({ "n", "x" }, "go", function()
+
+    vim.keymap.set("n", "<leader>oc", function()
+      require("opencode").toggle()
+    end, { desc = "Toggle embedded" })
+    vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+      require("opencode").ask("@this: ", { submit = true })
+    end, { desc = "Ask opencode" })
+    vim.keymap.set({ "n", "x" }, "<leader>og", function()
       require("opencode").prompt("@this")
     end, { desc = "Add to opencode" })
+    vim.keymap.set("n", "<leader>ob", function()
+      require("opencode").prompt("@buffer", { append = true })
+    end, { desc = "Add buffer to prompt" })
+    vim.keymap.set("n", "<leader>on", function()
+      require("opencode").command("session_new")
+    end, { desc = "New session" })
+    vim.keymap.set({ "n", "v" }, "<leader>os", function()
+      require("opencode").select()
+    end, { desc = "Select prompt" })
   end,
 }
