b8ce062bbb81244b53ff0344e5a9ac5e89cb7207
Kevin Montoya <me@kmontocam.com>
2025-12-06 17:10:22 -0600

    fix: match silicon config

--- nvim/lua/plugins/silicon.lua
+++ nvim/lua/plugins/silicon.lua
@@ -1,22 +1,26 @@
 return {
   "michaelrommel/nvim-silicon",
-  lazy = true,
-  cmd = "Silicon",
-  init = function()
-    vim.keymap.set("v", "<leader>sc", "<cmd>Silicon<cr>", { desc = "Snapshot Code" })
-  end,
-  config = function()
+  opts = {
+    font = "JetBrainsMono Nerd Font=34",
+    theme = "Visual Studio Dark+",
+    pad_horiz = 0,
+    pad_vert = 0,
+    window_title = function()
+      return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+    end,
+    output = function()
+      return os.getenv("HOME") .. "/Downloads/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
+    end,
+  },
+  config = function(_, opts)
     local silicon = require("nvim-silicon")
-    silicon.setup({
-      font = "JetBrainsMono Nerd Font=34",
-      theme = "Visual Studio Dark+",
-      background = "#FEFCF7",
-      window_title = function()
-        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
-      end,
-      output = function()
-        return os.getenv("HOME") .. "/Downloads/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
-      end,
-    })
+    silicon.setup(opts)
+
+    vim.keymap.set("v", "<leader>sc", function()
+      silicon.clip()
+    end, { desc = "Copy code screenshot to clipboard" })
+    vim.keymap.set("v", "<leader>sf", function()
+      silicon.file()
+    end, { desc = "Save code screenshot as file" })
   end,
 }
