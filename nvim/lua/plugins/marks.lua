bd46287d2dbe94eb147b590c626dd233b19802aa
Kevin Montoya <me@kmontocam.com>
2025-12-06 01:16:58 -0600

    feat: repeatable marks navigation

--- nvim/lua/plugins/marks.lua
+++ nvim/lua/plugins/marks.lua
@@ -9,6 +9,34 @@ return {
       preview = "m?",
     },
   },
+  config = function(_, opts)
+    require("marks").setup(opts)
+
+    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+    local marks = require("marks")
+
+    -- Setup repeatable moves using treesitter's repeatable_move
+    local next_mark, prev_mark = ts_repeat_move.make_repeatable_move_pair(function()
+      marks.next()
+    end, function()
+      marks.prev()
+    end)
+
+    vim.keymap.set({ "n", "x", "o" }, "]m", next_mark, { noremap = true, desc = "Next mark" })
+    vim.keymap.set({ "n", "x", "o" }, "[m", prev_mark, { noremap = true, desc = "Previous mark" })
+    vim.keymap.set(
+      { "n", "x", "o" },
+      ";",
+      ts_repeat_move.repeat_last_move,
+      { noremap = true, desc = "Next mark (repeat)" }
+    )
+    vim.keymap.set(
+      { "n", "x", "o" },
+      ",",
+      ts_repeat_move.repeat_last_move_opposite,
+      { noremap = true, desc = "Previous mark (repeat)" }
+    )
+  end,
   keys = {
     {
       "<leader>fm",
