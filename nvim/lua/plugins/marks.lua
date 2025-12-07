048e9fde7ee6ade90bd214536bb511c899299138
Kevin Montoya <me@kmontocam.com>
2025-12-06 19:49:20 -0600

    chore: marks to lazy keys

--- nvim/lua/plugins/marks.lua
+++ nvim/lua/plugins/marks.lua
@@ -4,43 +4,13 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
-  opts = {
-    mappings = {
-      set_next = "mm",
-      next = "]m",
-      prev = "[m",
-      preview = "m?",
-    },
-  },
-  config = function(_, opts)
-    require("marks").setup(opts)
-
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-    local marks = require("marks")
-
-    -- Setup repeatable moves using treesitter's repeatable_move
-    local next_mark, prev_mark = ts_repeat_move.make_repeatable_move_pair(function()
-      marks.next()
-    end, function()
-      marks.prev()
-    end)
-
-    vim.keymap.set({ "n", "x", "o" }, "]m", next_mark, { noremap = true, desc = "Next mark" })
-    vim.keymap.set({ "n", "x", "o" }, "[m", prev_mark, { noremap = true, desc = "Previous mark" })
-    vim.keymap.set(
-      { "n", "x", "o" },
-      ";",
-      ts_repeat_move.repeat_last_move,
-      { noremap = true, desc = "Next mark (repeat)" }
-    )
-    vim.keymap.set(
-      { "n", "x", "o" },
-      ",",
-      ts_repeat_move.repeat_last_move_opposite,
-      { noremap = true, desc = "Previous mark (repeat)" }
-    )
-  end,
   keys = {
+    { "mm", desc = "Set next mark" },
+    { "]m", desc = "Next mark" },
+    { "[m", desc = "Previous mark" },
+    { "m?", desc = "Preview mark" },
+    { ";", mode = { "n", "x", "o" }, desc = "Repeat last move" },
+    { ",", mode = { "n", "x", "o" }, desc = "Repeat last move opposite" },
     {
       "<leader>fm",
       function()
@@ -64,4 +34,40 @@ return {
       desc = "Find Marks",
     },
   },
+  opts = {
+    mappings = {
+      set_next = "mm",
+      next = false, -- disable default, will be set in config with repeatable move
+      prev = false,
+      preview = "m?",
+    },
+  },
+  config = function(_, opts)
+    require("marks").setup(opts)
+
+    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+    local marks = require("marks")
+
+    -- Setup repeatable moves using treesitter's repeatable_move
+    local next_mark, prev_mark = ts_repeat_move.make_repeatable_move_pair(function()
+      marks.next()
+    end, function()
+      marks.prev()
+    end)
+
+    vim.keymap.set({ "n", "x", "o" }, "]m", next_mark, { noremap = true, desc = "Next mark" })
+    vim.keymap.set({ "n", "x", "o" }, "[m", prev_mark, { noremap = true, desc = "Previous mark" })
+    vim.keymap.set(
+      { "n", "x", "o" },
+      ";",
+      ts_repeat_move.repeat_last_move,
+      { noremap = true, desc = "Repeat last move" }
+    )
+    vim.keymap.set(
+      { "n", "x", "o" },
+      ",",
+      ts_repeat_move.repeat_last_move_opposite,
+      { noremap = true, desc = "Repeat last move opposite" }
+    )
+  end,
 }
