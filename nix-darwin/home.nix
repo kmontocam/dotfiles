41b44dbec9f3028f3211794888d9b63506591ec9
Kevin Montoya <me@kmontocam.com>
2026-05-19 13:36:16 -0600

    feat: lazygit config

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -62,8 +62,10 @@
     ".tmux.conf".source = "${config.home.homeDirectory}/git/dotfiles/tmux/.tmux.conf";
     ".tool-versions".source = "${config.home.homeDirectory}/git/dotfiles/.tool-versions";
     "Library/Application Support/euporie".source = "${config.home.homeDirectory}/git/dotfiles/euporie";
-    "Library/Application Support/lazygit/config.yml".source =
-      "${config.home.homeDirectory}/git/dotfiles/lazygit/config.yml";
+    "Library/Application Support/lazygit/config.yml" = {
+      source = "${config.home.homeDirectory}/git/dotfiles/lazygit/config.yml";
+      force = true;
+    };
     ".config/fontconfig/fonts.conf".text = ''
       <?xml version="1.0"?>
       <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
