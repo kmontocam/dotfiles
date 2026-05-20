1bbdb3e7aa7e10e9a88407b67d03fa45197e8fbb
Kevin Montoya <me@kmontocam.com>
2026-05-20 13:04:52 -0600

    feat: syntax highlight injection python override at all levels

--- nvim/after/queries/python/highlights.scm
+++ nvim/after/queries/python/highlights.scm
@@ -2,9 +2,10 @@
 
 ; override string highlight on injected content with neutral text color.
 ; at equal priority, this overrides @string because it's a later pattern,
-; while injected markdown highlights (child tree) still override this.
-(module
-  (comment) @_lang
+; while injected language highlights (child tree) still override this.
+
+; inside module or block
+(_ (comment) @_lang
   .
   (expression_statement
     (assignment
@@ -12,10 +13,45 @@
         (string_content) @markup.raw)))
   (#lua-match? @_lang "^#%s*%a+"))
 
-(module
-  (comment) @_lang
+(_ (comment) @_lang
   .
   (expression_statement
     (string
       (string_content) @markup.raw))
+  (#lua-match? @_lang "^#%s*%a+"))
+
+; first statement in body: (function, class, for, while, with, try, else)
+(_ (comment) @_lang
+  body: (block
+    .
+    (expression_statement
+      (assignment
+        right: (string
+          (string_content) @markup.raw))))
+  (#lua-match? @_lang "^#%s*%a+"))
+
+(_ (comment) @_lang
+  body: (block
+    .
+    (expression_statement
+      (string
+        (string_content) @markup.raw)))
+  (#lua-match? @_lang "^#%s*%a+"))
+
+; first statement in consequence: (if, elif)
+(_ (comment) @_lang
+  consequence: (block
+    .
+    (expression_statement
+      (assignment
+        right: (string
+          (string_content) @markup.raw))))
+  (#lua-match? @_lang "^#%s*%a+"))
+
+(_ (comment) @_lang
+  consequence: (block
+    .
+    (expression_statement
+      (string
+        (string_content) @markup.raw)))
   (#lua-match? @_lang "^#%s*%a+"))
--- nvim/after/queries/python/injections.scm
+++ nvim/after/queries/python/injections.scm
@@ -1,17 +1,9 @@
 ;; extends
 
 ; Inject language into triple-quoted strings preceded by a comment with the language name
-;
-; Usage:
-;   # markdown
-;   """
-;   # Hello World
-;   This is **bold** text
-;   """
 
-; assignment: variable = """..."""
-(module
-  (comment) @injection.language
+; inside module or block (comment is sibling of expression_statement)
+(_ (comment) @injection.language
   .
   (expression_statement
     (assignment
@@ -19,11 +11,45 @@
         (string_content) @injection.content)))
   (#gsub! @injection.language "#%s*(.+)" "%1"))
 
-; standalone string: """..."""
-(module
-  (comment) @injection.language
+(_ (comment) @injection.language
   .
   (expression_statement
     (string
       (string_content) @injection.content))
+  (#gsub! @injection.language "#%s*(.+)" "%1"))
+
+; first statement in body: (function, class, for, while, with, try, else)
+(_ (comment) @injection.language
+  body: (block
+    .
+    (expression_statement
+      (assignment
+        right: (string
+          (string_content) @injection.content))))
+  (#gsub! @injection.language "#%s*(.+)" "%1"))
+
+(_ (comment) @injection.language
+  body: (block
+    .
+    (expression_statement
+      (string
+        (string_content) @injection.content)))
+  (#gsub! @injection.language "#%s*(.+)" "%1"))
+
+; first statement in consequence: (if, elif)
+(_ (comment) @injection.language
+  consequence: (block
+    .
+    (expression_statement
+      (assignment
+        right: (string
+          (string_content) @injection.content))))
+  (#gsub! @injection.language "#%s*(.+)" "%1"))
+
+(_ (comment) @injection.language
+  consequence: (block
+    .
+    (expression_statement
+      (string
+        (string_content) @injection.content)))
   (#gsub! @injection.language "#%s*(.+)" "%1"))
