45441a34c79809b8cecca4ae29b64ce68560fd76
Kevin Montoya <me@kmontocam.com>
2026-07-16 11:09:44 -0600

    feat: support for triple quoted strings sql injection

--- nvim/after/queries/python/injections.scm
+++ nvim/after/queries/python/injections.scm
@@ -1,22 +1,44 @@
 ; extends
 
 ; # sql
-; definition="""..."""
+; ...string in any position: assignment, kwarg, positional arg, bare expression,
+; or a method call on the string (e.g. """...""".strip()).
+; content+interpolation captured together so each string parses as ONE sql document
 ((comment) @_sql
   .
-  (keyword_argument
-    value: (string
-      (string_content) @injection.content))
+  [
+    (expression_statement
+      (assignment
+        right: (string
+          [
+            (string_content)
+            (interpolation)
+          ]+ @injection.content)))
+    (expression_statement
+      (string
+        [
+          (string_content)
+          (interpolation)
+        ]+ @injection.content))
+    (keyword_argument
+      value: (string
+        [
+          (string_content)
+          (interpolation)
+        ]+ @injection.content))
+    (string
+      [
+        (string_content)
+        (interpolation)
+      ]+ @injection.content)
+    (call
+      function: (attribute
+        object: (string
+          [
+            (string_content)
+            (interpolation)
+          ]+ @injection.content)))
+  ]
   (#lua-match? @_sql "^#%s*sql%s*$")
-  (#set! injection.language "sql"))
-
-; # sql
-; query = """..."""
-((comment) @_sql
-  .
-  (expression_statement
-    (assignment
-      right: (string
-        (string_content) @injection.content)))
-  (#lua-match? @_sql "^#%s*sql%s*$")
-  (#set! injection.language "sql"))
+  (#set! injection.language "sql")
+  (#set! injection.include-children))
